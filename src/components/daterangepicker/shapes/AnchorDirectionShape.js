aba0a1948d6cc9d037ccbc7f96008da7207cdd1e
Marklb <markberry867@gmail.com>
2016-10-24 08:09:17 -0400

    Fixed DateRangePicker import with example in Home component

--- src/components/daterangepicker/shapes/AnchorDirectionShape.js
+++
@@ -1,8 +0,0 @@
-import { PropTypes } from 'react';
-
-import {
-  ANCHOR_LEFT,
-  ANCHOR_RIGHT,
-} from '../constants/constants';
-
-export default PropTypes.oneOf([ANCHOR_LEFT, ANCHOR_RIGHT]);
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,53 +1,26 @@
-import _ from 'lodash';
 import React from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
-import TopHeader from '../top-header/top-header';
-import ModalsContainer from '../modals-container/modals-container';
-import LoginForm from '../login-form/login-form';
-import SignUpForm from '../signup-form/signup-form';
-import DateRangePicker from '../daterangepicker/jsx/DateRangePickerWrapper';
+// Javascript modules
+import _ from 'lodash';
+
+// React Components
+import {DateRangePicker} from 'react-dates';
 
 require("./home.scss");
 require("../top-header/top-header.scss");
 
 /*
-Root home component to the app
+Home page component
 */
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
 
-    // Probably not the best was to organized props, but works fine for now.
     this.state = {
-      // TODO: Make a proper implementation for logged in status
-      isLoggedIn: false,
-
-      // Some states intended to be only used by the modal components
-      modalVars: {
-        isVisible: {
-          // Only put booleans in this object
-          login_form: false,
-          signup_form: false
-        },
-      },
-      showModal: (modal_name) => {
-        let newState = this.state;
-        newState.modalVars.isVisible[modal_name] = true;
-        this.setState(newState);
-      },
-      hideModal: (modal_name) => {
-        let newState = this.state;
-        newState.modalVars.isVisible[modal_name] = false;
-        this.setState(newState);
-      },
-      hideAllModals: () => {
-        let newState = this.state;
-        _.forEach(newState.modalVars.isVisible, function(value, key) {
-            newState.modalVars.isVisible[key] = false;
-          });
-        this.setState(newState);
-      }
+      focusedInput: null,
+      startDate: null,
+      endDate: null,
     };
   }
 
@@ -56,36 +29,34 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <TopHeader {...this.state} />
+        <center><h1>Live There</h1></center>
 
-        {this.props.children}
+        <div className="search-bar-wrapper">
+          <form className="search-form">
 
-        <ModalsContainer {...this.state} >
-          <LoginForm name="login_form" isVisible={this.state.modalVars.isVisible.login_form} />
-          <SignUpForm name="signup_form" isVisible={this.state.modalVars.isVisible.signup_form} />
-        </ModalsContainer>
-	<center><h1>Live There</h1></center>
-
-          <div className="search-bar-wrapper">
-            <form className="search-form">
-
-              <div className="search-bar">
-                <div className="search-bar-icon"></div>
-                <input type="text" placeholder="Where to?"
-                  autoComplete="off"
-                  name="location"
-                  id="header-search-form"
-                  className="location"
-                  onKeyPress={this.onKeyPressSearchInput.bind(this)}
+            <div className="search-bar">
+              <div className="search-bar-icon"></div>
+              <input type="text" placeholder="Where to?"
+                autoComplete="off"
+                name="location"
+                id="header-search-form"
+                className="location"
+                onKeyPress={this.onKeyPressSearchInput.bind(this)}
                 />
 
-              </div>
+              <DateRangePicker
+                onDatesChange={this.onDatesChange.bind(this)}
+                onFocusChange={this.onFocusChange.bind(this)}
+                focusedInput={this.state.focusedInput}
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                />
 
-            </form>
-          </div>
-
+            </div>
+          </form>
         </div>
 
+      </div>
     );
   }
 
@@ -94,6 +65,13 @@ export default class Home extends React.Component {
   Event Callbacks
 
   */
+  onDatesChange({ startDate, endDate }) {
+    this.setState({ startDate, endDate });
+  }
+
+  onFocusChange(focusedInput) {
+    this.setState({ focusedInput });
+  }
 
   onKeyPressSearchInput(event){
     console.log('Clicked Enter');
