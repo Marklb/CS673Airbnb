85fb1866c1668fcab0dc954bd1f4d6de47eb05da
Marklb <markberry867@gmail.com>
2016-11-28 23:40:20 -0500

    Add: Upload user profile picture ui Add: /api/upload_user_profile_image

--- src/components/users-edit/sections/photos-and-video.js
+++ src/components/users-edit/sections/photos-and-video.js
@@ -1,6 +1,10 @@
-import _ from 'lodash';
 import React from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+
+import _ from 'lodash';
+import $ from 'jquery';
+import UserSessionHandler from '../../../user-session-handler';
+import ModalsHandler from '../../../modals-handler';
 
 import DashboardContainer from '../../dashboard-container';
 
@@ -11,6 +15,11 @@ require("../users-edit.scss");
 
 */
 export default class PhotosAndVideo extends React.Component {
+  static contextTypes = {
+    userSessionHandler: React.PropTypes.instanceOf(UserSessionHandler).isRequired,
+    modalsHandler: React.PropTypes.instanceOf(ModalsHandler).isRequired
+  };
+
   static propTypes = {
 
   };
@@ -22,6 +31,12 @@ export default class PhotosAndVideo extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      profilePhotoFile: '',
+      profilePhotoPreviewUrl: '/images/user_pic-225x225.png'
+    };
+
+    this.onChangeImageUpload = this.onChangeImageUpload.bind(this);
   }
 
   componentDidMount() {
@@ -32,11 +47,20 @@ export default class PhotosAndVideo extends React.Component {
     return (
       <div>
 
-        <div className="dashboard-panel">
+        <div className="dashboard-panel profile-photo-panel">
           <div className="dashboard-panel-header">Profile Photo</div>
           <div className="dashboard-panel-body">
+            <div className="profile-photo-body">
+              <div className="profile-photo-container">
+                <img className="profile-photo" 
+                     src={this.state.profilePhotoPreviewUrl} />
+              </div>
+              <div className="profile-photo-btns-container">
+                <input type="file" className="upload-photo-btn" 
+                      onChange={this.onChangeImageUpload}/>
+              </div>
 
-
+            </div>
           </div>
         </div>
 
@@ -50,7 +74,42 @@ export default class PhotosAndVideo extends React.Component {
   Event Callbacks
 
   */
+  onChangeImageUpload(e) {
+    e.preventDefault();
 
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = (evt) => {
+      if(file.size > 10000) {
+        console.log('File size to large.');
+        return;
+      }
+      if(file.type != 'image/png' && file.type != 'image/gif' 
+        && file.type != 'image/jpeg'){
+        console.log('File not correct type.');
+        return;
+      }
+      
+      let postData = this.context.userSessionHandler.getSessionAuthValues();
+      postData.imgData = evt.target.result;
+      $.post('/api/upload_user_profile_image', postData, (data, status) => {
+        if(data.success === false) {
+          console.log('Profile Image Upload Not Successful');
+        } else {
+          console.log('Profile Image Upload Successful');
+          this.setState({
+            profilePhotoFile: file,
+            profilePhotoPreviewUrl: reader.result
+          });
+        }
+      });
+
+      
+    }
+
+    reader.readAsDataURL(file)
+  }
 
 
 
